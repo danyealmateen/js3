cc140b14938fef587f1b701d300d2c3704433595
danyealmateen <danyeal.mateen@hotmail.com>
2023-10-03 15:23:58 +0200

    grammar updated

--- script.js
+++ script.js
@@ -1,124 +1,19 @@
-// 1
-// const a = {};
-// const b = { key: "b" };
-// const c = { key: "c" };
-// a[b] = 123;
-// a[c] = 456;
-// console.log(a[b]);
-
-//2
-// const nums = {
-//   num1: 500,
-//   num2: 1400,
-//   title: "my nums",
-// };
-
-// function multiplyByTwo(obj) {
-//   const newObj = { ...obj };
-
-//   for (let key in newObj) {
-//     if (typeof newObj[key] === "number") {
-//       newObj[key] *= 2;
-//     }
-//   }
-//   return newObj;
-// }
-// console.log(multiplyByTwo(nums));
-
-//3 3. Create a simple object calculator.
-// const calculator = {
-//   value1: null,
-//   value2: null,
-
-//   read(val1, val2) {
-//     this.value1 = val1;
-//     this.value2 = val2;
-//   },
-//   sum() {
-//     return this.value1 + this.value2;
-//   },
-//   mul() {
-//     return this.value1 * this.value2;
-//   },
-// };
-
-// calculator.read(2, 3);
-// console.log(calculator.sum());
-// console.log(calculator.mul());
-
-// const originalObj = {
-//   a: 1,
-//   b: 2,
-//   c: 3,
-// };
-
-// const newObj = {
-//   d: 4,
-//   e: 5,
-//   f: 6,
-// };
-
-// const ultraObj = { ...originalObj, ...newObj };
-// console.log(ultraObj);
-
-// const testObj = {
-//   name: "Dany",
-//   age: "32",
-//   location: "Sweden",
-// };
-
-// for (let key in testObj) {
-//   console.log(key, testObj[key]);
-//   console.log(typeof key);
-// }
-
-// const car = {
-//   brand: "lexus",
-//   model: "ls400",
-//   color: "green-metallic",
-// };
-
-// for (let key in car) {
-//   console.log(`${key}: ${car[key]}`);
-// }
-
-//4
-// const calc = {
-//   value1: null,
-//   value2: null,
-
-//   add() {
-//     return this.value1 + this.value2;
-//   },
-//   subtract() {
-//     return this.value1 - this.value2;
-//   },
-//   multiply() {
-//     return this.value1 * this.value2;
-//   },
-//   divide() {
-//     return this.value1 / this.value2;
-//   },
-// };
-
-// const results = calc.add(10).subtract(4).multiply(5).divide(9).add(10);
-
 //JAVASCRIPT 3 GRAMMAR
-//1.
+
+//1.Palindrome övning
 function checkIfPalindrome(word) {
   word = word.toLowerCase();
-  //Split delar upp ordet till en array av karaktärer, när det blir en array kan man använda metoden reverse för att vända ordningen på elementen i arrayen och sen join för att sätta ihop karaktärerna till en vanlig sträng igen.
   const reversed = word.split("").reverse().join("");
 
   if (word === reversed) {
-    console.log(true);
+    console.log("Palindrome");
   } else {
-    console.log(false);
+    console.log("Not Palindrome");
   }
 }
-checkIfPalindrome("level");
+// checkIfPalindrome("hannah");
 
-//2.
+//2.Capitalize the first letter of each word in a sentence
 function capsFirstLetter(word) {
   const words = word.split(" ");
   const capsFirstWord = words.map((word) => {
@@ -128,6 +23,130 @@ function capsFirstLetter(word) {
     return newWord;
   });
   const capitalizedSentence = capsFirstWord.join(" ");
-  console.log(capitalizedSentence);
+  // console.log(capitalizedSentence);
 }
 capsFirstLetter("send me the money");
+
+//3.Reverse a string
+function reverseString(word) {
+  const reversedString = word.split("").reverse().join("");
+  return reversedString;
+}
+reverseString("hello");
+
+//4.Loop through a string of character
+function isLetter(stringy) {
+  const regex = /^[A-Za-z]$/;
+  const vowels = "aeiou";
+  const chars = stringy.split("");
+  let result = chars.map((char) => {
+    //testa char mot regex för att se om det är en bokstav
+    if (regex.test(char)) {
+      let nextChar;
+      //om char är större eller lika med charen "a" och mindre eller lika med z så returna en jämförelse där man kollar om char är z isånafall ska den returnera a annars konvertera char i detta fallet till ett ASCII (american standard code for information interchange) värde som blir en siffra, t,ex 'A' har värdet 65, 'B' har värdet 66 och plussa på med 1.
+      if (char >= "a" && char <= "z") {
+        nextChar =
+          char === "z" ? "a" : String.fromCharCode(char.charCodeAt(0) + 1);
+      } else if (char >= "A" && char <= "Z") {
+        //samma sak som ovan fast med stora versaler
+        nextChar =
+          char === "Z" ? "A" : String.fromCharCode(char.charCodeAt(0) + 1);
+      }
+      if (vowels.includes(nextChar)) {
+        nextChar = nextChar.toUpperCase();
+      }
+
+      return nextChar;
+    } else {
+      //returnera char oförändrat om det inte är en bokstav
+      return char;
+    }
+  });
+  return result.join("");
+}
+// console.log(isLetter("Javascript3"));
+
+//5
+function validatePara(val) {
+  const regex = /^[a-zA-Z0-9_]+$/;
+  const startCharRegex = /^[a-zA-Z]/;
+
+  if (!regex.test(val)) {
+    console.log(
+      "Not a valid string. Only alphanumerical characters and underscore are allowed."
+    );
+    return false;
+  }
+
+  if (!startCharRegex.test(val)) {
+    console.log("Invalid first character. It must be a letter a-z/A-Z.");
+    return false;
+  }
+
+  if (val.slice(-1) === "_") {
+    console.log("The last character cannot be an underscore (_).");
+    return false;
+  }
+
+  if (val.length < 4) {
+    console.log("Need more characters");
+    return false;
+  } else if (val.length > 25) {
+    console.log("Too many characters");
+    return false;
+  }
+  console.log("Parameter is valid");
+  falseOrTrue = true;
+  return true;
+}
+
+// const validationResult = validatePara("javascript");
+// console.log(validationResult);
+
+//6
+function countOccurences(char, str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      console.log("str[i]:", str[i]);
+      count++;
+    }
+  }
+  return count;
+}
+// console.log(countOccurences("a", "jaaaavascript"));
+
+//7
+function longestWord(str) {
+  const splittedWord = str.split(" ");
+  let longest = "";
+
+  splittedWord.map((word) => {
+    const onlyLetters = /^[a-zA-Z]/;
+    if (onlyLetters.test(word) && word.length > longest.length) {
+      longest = word;
+    }
+  });
+  return longest;
+}
+console.log(longestWord("Hello my name is JavascriptSlayer99"));
+
+//8
+function duplicatedValues(arr) {
+  let occurrences = {};
+  let duplicates = [];
+
+  for (let item of arr) {
+    if (occurrences[item]) {
+      if (occurrences[item] === 1) {
+        duplicates.push(item);
+      }
+      occurrences[item]++;
+    } else {
+      occurrences[item] = 1;
+    }
+  }
+  return duplicates;
+}
+console.log(duplicatedValues([1, 2, 2, 3, 3, "hi", "hi"]));
